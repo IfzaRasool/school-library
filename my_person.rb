06591099ac8896869d0a5a3cab32ae202cfeb9f4
IfzaRasool <ifzarasoolarain@gmail.com>
2022-06-13 17:47:17 +0300

    fix error

--- my_person.rb
+++ my_person.rb
@@ -1,44 +1,19 @@
 class MyPerson
-  def initialize(age, parent_permission: true, name = "Unknown")
+  attr_reader :id
+  attr_accessor :name, age
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
-  # getter methods
-  def get_id
-    @id
-  end
-
-  def get_name
-    @name
-  end
-
-  def get_age
-    @age
-  end
-
-  # setter
-  def set_name=(value)
-    @name = value
-  end
-
-  def set_age=(value)
-    @age = value
-  end
-
-  def is_of_age
-    if @age >= 18
-      true
-    else
-      false
-    end
+  def of_age?
+    @age >= 18
   end
   private :is_of_age
-  def can_use_services
-    if is_of_age and @parent_permission
-      puts true
-    end
+  def can_use_services?
+    @parent_permission || of_age?
   end
 end
--- my_teacher.rb
+++ my_teacher.rb
@@ -1,4 +1,4 @@
-require_relative './Person'
+require_relative './my_person'
 
 class MyTeacher < MyPerson
   def initialize(specialization)
