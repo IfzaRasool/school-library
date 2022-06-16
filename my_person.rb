d1c27d89cd55a308889e181a4675de02a9607484
IfzaRasool <ifzarasoolarain@gmail.com>
2022-06-16 19:07:35 +0300

    add app and main

--- my_person.rb
+++ my_person.rb
@@ -72,9 +72,9 @@ class TrimmerDecorator < CapitalizeDecorator
   end
 end
 
-person = MyPerson.new(22, 'maximilian')
-person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-capitalized_trimmed_person.correct_name
+# person = MyPerson.new(22, 'maximilian')
+# person.correct_name
+# capitalized_person = CapitalizeDecorator.new(person)
+# capitalized_person.correct_name
+# capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+# capitalized_trimmed_person.correct_name
